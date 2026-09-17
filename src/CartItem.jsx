e799237d4b7d6934de06172205f414929fef48b2
TJ-hazard <tejirigbiyede@gmail.com>
2026-09-17 09:15:57 -0400

    Final Project

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -9,27 +9,48 @@ const CartItem = ({ onContinueShopping }) => {
 
   // Calculate total amount for all products in the cart
   const calculateTotalAmount = () => {
- 
+    let total = 0;
+
+    cart.forEach((item) => {
+    const { quantity, cost } = item;
+    const price = parseFloat(cost.substring(1));
+    total += price * quantity;
+  });
+
+  return total;
   };
 
   const handleContinueShopping = (e) => {
-   
+    alert('Functionality to be added for future reference');
   };
 
 
 
   const handleIncrement = (item) => {
+    dispatch(updateQuantity({ name: item.name, quantity: item.quantity + 1 }))
   };
 
   const handleDecrement = (item) => {
-   
+   if(item.quantity >1){
+     dispatch(updateQuantity({name: item.name, quantity: item.quantity - 1 }))
+   } 
+   else if (item.quantity == 0) {
+    dispatch(removeItem(item.name))
+   }
   };
 
   const handleRemove = (item) => {
+    dispatch(removeItem(item.name))
   };
 
   // Calculate total cost based on quantity for an item
   const calculateTotalCost = (item) => {
+    let total = 0
+    const {cost,quantity} = item
+    const price = parseFloat(cost.substring(1))
+    total += price * quantity
+
+    return total
   };
 
   return (
